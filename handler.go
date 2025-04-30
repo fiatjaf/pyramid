8b86821d9e945954389137fcca7424867956d792
fiatjaf <fiatjaf@gmail.com>
2025-04-29 22:39:54 -0300

    experimental forum.

--- handler.go
+++ handler.go
@@ -91,3 +91,29 @@ func reportsViewerHandler(w http.ResponseWriter, r *http.Request) {
 
 	reportsPage(events, getLoggedUser(r)).Render(r.Context(), w)
 }
+
+func forumHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>forum</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/relay-forum@0.0.2/dist/index.css" />
+    <meta name="base-path" content="/forum" />
+  </head>
+  <body
+    class="bg-slate-100 transition-colors duration-200 dark:bg-gray-900 dark:text-white"
+  >
+    <div id="app"></div>
+  </body>
+  <script src="https://cdn.jsdelivr.net/npm/relay-forum@0.0.2/dist/index.js"></script>
+</html>
+`)
+}
--- main.go
+++ main.go
@@ -135,6 +135,7 @@ func main() {
 	relay.Router().HandleFunc("/remove-from-whitelist", removeFromWhitelistHandler)
 	relay.Router().HandleFunc("/cleanup", cleanupStuffFromExcludedUsersHandler)
 	relay.Router().HandleFunc("/reports", reportsViewerHandler)
+	relay.Router().HandleFunc("/forum/", forumHandler)
 	relay.Router().Handle("/static/", http.FileServer(http.FS(static)))
 	relay.Router().HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		if s.RelayIcon != "" {
