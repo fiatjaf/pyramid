8c6ee70400676ec8a5bab1d4d4245913db908a76
fiatjaf <fiatjaf@gmail.com>
2025-11-20 10:02:10 -0300

    grasp: differentiate repositories that have source code pushed from those that don't.

--- grasp/repos.go
+++ grasp/repos.go
@@ -2,17 +2,27 @@ package grasp
 
 import (
 	"iter"
+	"os"
+	"path/filepath"
 
 	"fiatjaf.com/nostr"
 	"fiatjaf.com/nostr/nip34"
 	"github.com/fiatjaf/pyramid/global"
 )
 
-func getRepositories() iter.Seq[nip34.Repository] {
-	return func(yield func(nip34.Repository) bool) {
+func getRepositories() iter.Seq2[nip34.Repository, bool] {
+	return func(yield func(nip34.Repository, bool) bool) {
 		for evt := range global.IL.Main.QueryEvents(nostr.Filter{Kinds: []nostr.Kind{nostr.KindRepositoryAnnouncement}}, 1000) {
 			repo := nip34.ParseRepository(evt)
-			yield(repo)
+			var cont bool
+			if info, err := os.Stat(filepath.Join(repoDir, repo.ID)); err == nil && info.IsDir() {
+				cont = yield(repo, true)
+			} else {
+				cont = yield(repo, false)
+			}
+			if !cont {
+				return
+			}
 		}
 	}
 }
