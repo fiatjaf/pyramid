35c1567293678b8dd79de1484a2fc53dba2025b5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-07-21 03:18:17 +0900

    guard nprofile decode error before type assertion in PubKeyFromInput

--- global/utils.go
+++ global/utils.go
@@ -47,7 +47,7 @@ func PubKeyFromInput(input string) nostr.PubKey {
 	var pubkey nostr.PubKey
 	if pfx, value, err := nip19.Decode(input); err == nil && pfx == "npub" {
 		pubkey = value.(nostr.PubKey)
-	} else if pfx == "nprofile" {
+	} else if err == nil && pfx == "nprofile" {
 		pubkey = value.(nostr.ProfilePointer).PublicKey
 	} else if pk, err := nostr.PubKeyFromHex(input); err == nil {
 		pubkey = pk
